2cacd12fe7781d9728db46e5e48eeb252bd79aa3
Sai-cod <ekbotesairaj640@gmail.com>
2024-10-02 09:01:15 +0530

    1 step forward

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Welcome to Tic Tac Toe');
+
+let counter = 0;
+
+document.getElementById('play').addEventListener('click',()=>{
+    Array.from(document.getElementsByClassName('value')).forEach((element)=>{
+        element.getElementsByTagName('p')[0].innerHTML = ' ';
+    })
+    if(counter%2==0) {
+        document.getElementsByClassName('playturn').innerHTML = "X's";
+    }
+    else {
+        document.getElementsByClassName('playturn').innerHTML = "O's";
+    }
+})
+
+
+
+
